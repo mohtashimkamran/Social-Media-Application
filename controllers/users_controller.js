af23ebe6ef4340924a553505537c1ec7806be213
Mohtashim Kamran <mk.mohtashim@gmail.com>
2021-08-12 16:20:57 +0530

    friends

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,8 +2,11 @@ const User = require('../models/user');
 
 module.exports.profile = function(req,res){
     // return res.end('<h1>User Profile</h1>')
-    return res.render('profile',{
-        title:"Profile"
+    User.findById(req.params.id,function(err,user){
+        return res.render('profile',{
+            title:"Profile",
+            profile_user : user,
+    })
     });
 }
 
