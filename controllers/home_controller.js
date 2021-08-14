f462cfb2ade26d3458a18e225f89fd9b31ec53cf
Mohtashim Kamran <mk.mohtashim@gmail.com>
2021-08-14 15:56:02 +0530

    async/await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,33 +1,25 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    // return res.end("<h1> Express is up for Codezone</h1>")
-    // console.log(req.cookies);
-    // res.cookie('user_id',25);
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title : "CodeZone Home",
-    //         posts : posts
-    //     });
-    // });
-    //populate the user for each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user',
-        }
-    })
-    .exec(function(err,posts){
+module.exports.home = async function(req,res){
+    try {
+        //populate the users of each posts
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user',
+            }
+        })
+        let users = await User.find({});
 
-        User.find({},function(err,users){
-            return res.render('home',{
-                title : "CodeZone Home",
-                posts : posts,
-                all_users : users,
-            });
-        });
-    });
+        return res.render('home',{
+            title : "CodeZone Home",
+            posts : posts,
+            all_users : users,
+        });  
+    } catch (error) {
+        console.log("Error",error)
+    }
 }
