1696536d73c6d94be54f0f9133162e0f5a9c0ac4
Mohtashim Kamran <mk.mohtashim@gmail.com>
2021-09-04 01:40:38 +0530

    Deletion using API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,6 @@
 const Post = require('../../../models/post');
+const Comment =require('../../../models/comment');
+
 module.exports.index =async function(req,res){
     let posts = await Post.find({})
         .sort('-createdAt')
@@ -14,4 +16,19 @@ module.exports.index =async function(req,res){
         message:"List of Posts",
         posts: posts
     })
+}
+module.exports.destroy= async function(req,res){
+    try {
+        let post = await Post.findById(req.params.id)
+            post.remove();
+            await Comment.deleteMany({post:req.params.id});
+            return res.json(200,{
+                message:'Post and associated Comments Deleted Successfully',
+            })
+    } catch (error) {
+        console.log(error);
+        return res.json(500,{
+            message: 'Internal Server Error'
+        })
+    }
 }
